cc54a0ee774f79921e745dc38b781ec4bb8f631f
Zhen <zhengong.mcmaster@gmail.com>
2023-10-01 16:14:57 +0800

    bug: digital menu style lost when leaving, and no default on 1st element

--- src/main.js
+++ src/main.js
@@ -29,3 +29,40 @@ window.addEventListener('scroll', function() {
     }
   });
   
+const menus = document.querySelectorAll('.menus div')
+const activeItems = document.querySelectorAll('.activeItem')
+
+activeItems.forEach((item,index)=>{
+  if(index!==0){
+    item.style.display = 'none';
+  } else {
+    item.style.display = 'flex';
+  }
+});
+
+// menus.forEach((item,index)=>{
+//   if(index!==0){
+//     item.style.color = '#51565d';
+//     item.style.background ='linear-gradient(90deg,#eef4ff 4.15%,#f8faff)';
+//   } else {
+//     item.style.color = '#fff';
+//     item.style.background ='linear-gradient(180deg,#1977fe,#559bff)';
+//   }
+// });
+
+menus.forEach((menu, index) => {
+  menu.addEventListener('mouseover', ()=>{
+
+    activeItems.forEach(item=>{
+      item.style.display = 'none';
+    });
+    activeItems[index].style.display = 'flex';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  menus[0].classList.add('digitalMenuActive'); // 添加 hover 样式类
+});
+
+
